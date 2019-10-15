89c17dd894745f71648b6a51564ba1ecc9a91da3
Mertyce <mdoleman15@gmail.com>
2019-10-14 21:09:28 -0400

    get-it-done prep work assignment 1-6

--- main.py
+++ main.py
@@ -3,9 +3,10 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://get-it-done:password@localhost:8889/get-it-done'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://get-it-done:password@localhost:3306/get-it-done'
 app.config['SQLALCHEMY_ECHO'] = True
 db = SQLAlchemy(app)
+
 
 class Task(db.Model):
 
@@ -18,6 +19,55 @@ class Task(db.Model):
         self.completed = False
 
 
+class User(db.Model):
+
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(120), unique=True)
+    password = db.Column(db.String(120))
+
+    def __init__(self, email, password):
+        self.email = email
+        self.password = password
+
+
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+        user = User.query.filter_by(email=email).first()
+        if user and user.password == password:
+            # TODO - "remember" that the user has logged in
+            return redirect('/')
+        else:
+            # TODO - explain why login failed
+            return '<h1>Error!</h1>'
+
+    return render_template('login.html')
+
+
+@app.route('/register', methods=['POST', 'GET'])
+def register():
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+        verify = request.form['verify']
+
+        # TODO - validate user's data
+
+        existing_user = User.query.filter_by(email=email).first()
+        if not existing_user:
+            new_user = User(email, password)
+            db.session.add(new_user)
+            db.session.commit()
+            # TODO - "remember" the user
+            return redirect('/')
+        else:
+            # TODO - user better response messaging
+            return "<h1>Duplicate user</h1>"
+
+    return render_template('register.html')
+
 
 @app.route('/', methods=['POST', 'GET'])
 def index():
@@ -28,10 +78,11 @@ def index():
         db.session.add(new_task)
         db.session.commit()
 
-
-    tasks= Task.query.filter_by(completed=False).all()
+    tasks = Task.query.filter_by(completed=False).all()
     completed_tasks = Task.query.filter_by(completed=True).all()
-    return render_template('todos.html',title="Get It Done!", tasks=tasks, completed_tasks=completed_tasks)
+    return render_template('todos.html',title="Get It Done!", 
+        tasks=tasks, completed_tasks=completed_tasks)
+
 
 @app.route('/delete-task', methods=['POST'])
 def delete_task():
@@ -43,6 +94,7 @@ def delete_task():
     db.session.commit()
 
     return redirect('/')
+
 
 if __name__ == '__main__':
     app.run()
